ef91be0b235c3241284abc242e5a2f67279a685f
Ariella <ariella.yamada@sienaidea.com.br>
2017-10-02 09:45:10 -0300

    Transformando classes

--- index.android.js
+++ index.android.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Text, AppRegistry } from 'react-native'
+import { Text, View, AppRegistry } from 'react-native'
 import Header from './src/components/header'
+import AlbumList from './src/components/AlbumList'
 
 const App = () => (
-  <Header headerText={'Albuns'}/>
+  <View>
+    <Header headerText={'Albuns'}/>
+    <AlbumList />
+  </View>
 )
 
 AppRegistry.registerComponent('Exemplo', () => App)
